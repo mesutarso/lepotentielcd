25649fab3269ea41e705eeb10ffeec228f1a9b12
mechack tambwe <mesutarso@users.noreply.github.com>
2022-02-15 15:43:20 +0100

    fix ogo image

--- src/components/Shared/Logo.jsx
+++ src/components/Shared/Logo.jsx
@@ -1,13 +1,10 @@
 import React from "react";
+import LogoImage from "../../images/logo_Le_Potentiel_white.png";
 
 export default function Logo() {
   return (
     <div>
-      <img
-        src="static/logo-potentiel-white.png"
-        alt="logo"
-        className="img-fluid"
-      />
+      <img src={LogoImage} alt="logo" className="img-fluid" />
     </div>
   );
 }
