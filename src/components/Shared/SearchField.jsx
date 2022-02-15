dd509b4270adeac5950a0268c291738987571182
mechack tambwe <mesutarso@users.noreply.github.com>
2022-02-15 14:43:46 +0100

    add  search field

---
+++ src/components/Shared/SearchField.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Iconify from "./Iconify";
+import styled from "styled-components";
+
+const InputField = styled.form`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  .search-control {
+    padding: 8px 16px;
+    width: 80%;
+    border-radius: 24px;
+    border: none;
+    background-color: ${(props) => props.theme.colors.grey};
+    color: ${(props) => props.theme.colors.secondary};
+    font-size: 14px;
+    line-height: 16.41px;
+    font-weight: 500;
+    ::placeholder {
+      color: ${(props) => props.theme.colors.variant};
+    }
+  }
+`;
+
+export default function SearchField() {
+  return (
+    <InputField>
+      <input type="search" placeholder="Recherhe..." className="search-control" />
+      <Iconify icon="ant-design:search-outlined" />
+    </InputField>
+  );
+}
