7ea023003fcce21524e434fc417c510919aa16e9
mechack tambwe <mesutarso@users.noreply.github.com>
2022-02-15 15:29:15 +0100

    fix style components issues

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Seo from "./Seo";
+import { GlobalStyle } from "../../styles/global";
 
 export default function Layout({ children }) {
   return (
     <>
+      <GlobalStyle />
       <Seo />
       <Header />
       {children}
