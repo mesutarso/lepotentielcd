584be18f52620a69922259ded9b7495f2b67e770
mechack tambwe <mesutarso@users.noreply.github.com>
2022-02-15 12:22:08 +0100

    add some colors

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,8 +1,9 @@
 export const theme = {
-    colors: {
-        primary: '#E5232C',
-        secondary: '#000000',
-        tertiary: '#FFFFFF',
-        variant: '#ff001014',
-    },
-}
+  colors: {
+    primary: "#E5232C",
+    secondary: "#000000",
+    tertiary: "#FFFFFF",
+    variant: "#ff001014",
+    grey: "#F5F6F7",
+  },
+};
