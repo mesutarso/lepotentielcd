281f29e35675e6e0fd870ae7befdaee7e33da0f0
unknown <contactmampbeni@gmail.com>
2022-02-23 01:47:08 +0100

    feat: add cards

--- src/components/Shared/cards/thirdCard.jsx
+++ src/components/Shared/cards/thirdCard.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import styled from 'styled-components'
 import { Col, Row } from "react-bootstrap";
 
-const Title= styled.div`
+const Title= styled.h3`
 font-weight:700;
 font-size:13px !important;
 `
+const Datation=styled.div`
+ position:absolute;
+ bottom:0;
+ font-size:13px;
+`
+
 const ThirdCard=()=>{
     return (
         <>
@@ -13,11 +19,9 @@ const ThirdCard=()=>{
                 <Col>
                 <img src="../img.jpg" className="img-fluid" alt="sport" /> 
                 </Col>
-                <Col>
-                <Title>
-                        Sport : barcelone champion UEFA primary, ipsum dolor sit amet
-                        consectetur adipisicing elit.
-                    </Title>
+                <Col className="position-relative pb-0">
+                    <Title>Sport : barcelone champion UEFA primary, ipsum dolor sit amet</Title>
+                    <Datation className="p-0 mb-0 text-muted">22-04-2022</Datation>   
                 </Col>
                    
             </Row>
